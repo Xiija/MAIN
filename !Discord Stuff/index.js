8929f2c419968565ebfbfeffd760e214bbc910f8
Xiija <36306194+Xiija@users.noreply.github.com>
2025-01-26 18:08:14 -0800

    Update index.js

--- !Discord Stuff/index.js
+++ !Discord Stuff/index.js
@@ -6,35 +6,36 @@ let err = document.getElementById("d1");
 btn.addEventListener("click", doit, false);
 async function doit() {  
   err.innerText += 'inside the handler... \n';
-   await fetch( url, {
-      method: 'POST',
-      mode: "cors",
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-       username: "sum fool" ,     
-       content:   ".\n"  + mess
-      })
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-         err.innerText += 'Network response was not ok \n';
-      }
-      return response.json(); // or response.text() if the response is not JSON
-    })
-    .then(data => {
-      // Handle the response data
-      console.log(data);
-         err.innerText += 'Data response: \n' + data;
-    })
-    .catch(error => {
-      // Handle any errors
-      console.error('Error:', JSON.stringify(error) );
-      err.innerText += 'ERRER \n' + JSON.stringify(error);
-    });
+  
+   // await fetch( url, {
+   //    method: 'POST',
+   //    mode: "cors",
+   //    headers: {
+   //      'Content-Type': 'application/json',
+   //      'Access-Control-Allow-Origin': '*'
+   //    },
+   //    body: JSON.stringify({
+   //     username: "sum fool" ,     
+   //     content:   ".\n"  + mess
+   //    })
+   //  })
+   //  .then(response => {
+   //    if (!response.ok) {
+   //      throw new Error('Network response was not ok');
+   //       err.innerText += 'Network response was not ok \n';
+   //    }
+   //    return response.json(); // or response.text() if the response is not JSON
+   //  })
+   //  .then(data => {
+   //    // Handle the response data
+   //    console.log(data);
+   //       err.innerText += 'Data response: \n' + data;
+   //  })
+   //  .catch(error => {
+   //    // Handle any errors
+   //    console.error('Error:', JSON.stringify(error) );
+   //    err.innerText += 'ERRER \n' + JSON.stringify(error);
+   //  });
   
 };
-//doit();
+
